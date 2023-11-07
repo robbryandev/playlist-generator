4bc5a68f8d2e934ef41f6971f25d7b090552a98d
robbryandev <robbryandev@gmail.com>
2023-11-07 10:44:33 -0800

    Add tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel</title>
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+  @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased">
+  <p class="text-lg text-center text-blue-600">Hello, World!</p>
+</body>
+
+</html>
